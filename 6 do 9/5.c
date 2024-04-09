058d30c5b8794ec8789232afebd06320cc1c73a2
Thiago Vinicius <67398127+ThiagoVReznde@users.noreply.github.com>
2024-04-09 10:14:56 -0300

    Create 5.c

---
+++ 6 do 9/5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+float S, Sx, SM, P, Fx;
+int n, F;
+
+int main(){
+    for(n=0; n<3; ++n) {
+
+        printf("\nSalario %d: ", n);
+        scanf("%f", &S);
+        printf("\nFilhos %d: ", n);
+        scanf("%d", &F);
+    
+        if(S<=150)
+        ++P;
+        if(S>SM)
+        SM=S;
+        Sx+=S;
+        Fx+=F;
+    }
+Sx=(Sx/n);
+Fx=(Fx/n);
+P=(P/n);
+printf("\nMedia Salario: R$%.2f;\nMedia Filhos: %.1f;\nMaior Salario: %.2f por cento;\nPercentil Pobres: %.2f\n", Sx, Fx, SM, (P*100));
+return(1);
+}
